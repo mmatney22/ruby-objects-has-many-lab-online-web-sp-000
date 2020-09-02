7b314e3449617f7e6c8af926b07f6b7d9b8c22ce
Morgan Matney <morgan.matney89@gmail.com>
2020-09-02 22:32:48 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,18 @@
+class Post
+  attr_accessor :author, :title
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    if self.author.name == nil ? nil : self.author.name
+    end
+  end
+end
